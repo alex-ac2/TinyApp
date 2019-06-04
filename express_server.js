8381c60eeefb038952e849e4a32826c6a4ebc709
Alex Curci <alex.curci@gmail.com.com>
2019-06-04 18:43:55 +0000

    Initial add of views folder for EJS view engine and updated 'urls' request listener

--- express_server.js
+++ express_server.js
@@ -1,8 +1,26 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+const express = require("express");
+const app = express();
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+let  urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
+app.get("/hello", (req, res) => {
+  let templateVarHello = { greeting: 'Why hello friend' };
+  res.render("hello_world", templateVarHello);
+  console.log("Hello response sent.");
+});
+
+// Initiate app using PORT variable
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
