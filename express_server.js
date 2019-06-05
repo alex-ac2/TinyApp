3a7cfa4c3103bb3af0b3025280c42c05620a18e0
Alex Curci <alex.curci@gmail.com.com>
2019-06-05 02:59:44 +0000

    Added route comments

--- express_server.js
+++ express_server.js
@@ -12,16 +12,19 @@ let  urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Generate random string for URL key
 function generateRandomString() {
   let randomID = Math.random().toString(36).substr(3, 6);
   return randomID;
 }
 
+// URL index - list all shortened URLs
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Save user inputed shortened URLs and redirect 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   let generatedID = generateRandomString();
@@ -31,10 +34,19 @@ app.post("/urls", (req, res) => {
   res.redirect(301,"/urls/" + generatedID);
 });
 
+// Page to input to shortened URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Redirect route from shortened URL to website
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]; 
+  console.log("Redirection occured.");
+  res.redirect(longURL);
+});
+
+// Shortened URL display page
 app.get("/urls/:shortURL", (req, res) => {
   console.log(req.params.shortURL);
   let templateVars = { 
@@ -43,6 +55,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// Test hello route
 app.get("/hello", (req, res) => {
   let templateVarHello = { greeting: 'Why hello friend' };
   res.render("hello_world", templateVarHello);
